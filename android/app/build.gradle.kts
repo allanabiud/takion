0545b328543a4b1ab6a480e01050f1449c87b494
allanabiud <allanabiud@gmail.com>
2026-07-06 12:06:52 +0300

    rename Android package to com.akasha.takion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.takion.takion"
+    namespace = "com.akasha.takion"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.takion.takion"
+        applicationId = "com.akasha.takion"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/akasha/takion/MainActivity.kt
+++ android/app/src/main/kotlin/com/akasha/takion/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.takion.takion
+package com.akasha.takion
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
